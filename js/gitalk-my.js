3757a7df352b1481ff6a5b6f5a77f82aae7afb3d
kirito <1026860069@qq.com>
2020-04-21 11:59:36 +0800

    尝试解决gitalk ：Validation Failed 问题

---
+++ js/gitalk-my.js
@@ -0,0 +1,20 @@
+const gitalk = new Gitalk({
+    clientID: 'ced6988e6ccedf962b67',
+    clientSecret: '73a32399b7eda1c3142aa163775550aa214b056a',
+    repo: 'zshorz.github.io',
+    owner: 'zshorz',
+    admin: ['zshorz'],
+    id: hex_md5(hex_md5(window.location.pathname + window.location.hash)),
+    distractionFreeMode: true
+});
+
+function lablegen(hook) {
+    var dom = Docsify.dom;
+
+    hook.doneEach(function (_) {
+        gitalk.id = hex_md5(hex_md5(window.location.pathname + window.location.hash))
+        //console.log(gitalk.id)
+    });
+}
+//
+$docsify.plugins = [].concat(lablegen, $docsify.plugins);
