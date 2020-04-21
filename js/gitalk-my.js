d7c0a2e1de32bee0496d942565828306c73c0e59
kirito <1026860069@qq.com>
2020-04-21 12:49:32 +0800

    test

--- js/gitalk-my.js
+++ js/gitalk-my.js
@@ -1,18 +1,16 @@
-gitalk = new Gitalk({
-    clientID: 'ced6988e6ccedf962b67',
-    clientSecret: '73a32399b7eda1c3142aa163775550aa214b056a',
-    repo: 'zshorz.github.io',
-    owner: 'zshorz',
-    admin: ['zshorz'],
-    id: hex_md5(hex_md5(window.location.pathname + window.location.hash)),
-    distractionFreeMode: true
-});
+
 
 function lablegen(hook) {
-    var dom = Docsify.dom;
-
     hook.doneEach(function () {
-        gitalk.id = hex_md5(hex_md5(window.location.pathname + window.location.hash))
+        gitalk = new Gitalk({
+            clientID: 'ced6988e6ccedf962b67',
+            clientSecret: '73a32399b7eda1c3142aa163775550aa214b056a',
+            repo: 'zshorz.github.io',
+            owner: 'zshorz',
+            admin: ['zshorz'],
+            id: hex_md5(hex_md5(window.location.pathname + window.location.hash)),
+            distractionFreeMode: true
+        });
         console.log(gitalk.id)
     });
 }
