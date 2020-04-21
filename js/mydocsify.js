eaf6510acd4ff15a8d1c41a973a86909588201a9
kirito <1026860069@qq.com>
2020-04-21 11:07:13 +0800

    使用定制的插件加载代码，官方的在封面加载有问题

--- js/mydocsify.js
+++ js/mydocsify.js
@@ -5,6 +5,7 @@ window.$docsify = {
   loadNavbar: true, // 导航栏
   coverpage: true, // 封面
   onlyCover: true, // 只显示封面，不显示文章
+  auto2top: true, //  切换页面自动顶部
 
   //autoHeader: true,
 
@@ -33,5 +34,46 @@ window.$docsify = {
   //   // },
   //   depth: 2,                       // 搜索标题的最大程级, 1 - 6
   // }
+
+  // 插件的各个回调
+  // plugins: [
+  //   function (hook, vm) {
+  //     hook.init(function() {
+  //       // 初始化时调用，只调用一次，没有参数。
+  //       console.debug("init")
+  //     })
+  
+  //     hook.mounted(function() {
+  //       // 初始化完成后调用，只调用一次，没有参数。
+  //       console.debug("mounted")
+  //     })
+  
+  //     hook.beforeEach(function(content) {
+  //       // 每次开始解析 Markdown 内容时调用
+  //       // ...
+  //       console.debug("beforeEach")
+  //       return content
+  //     })
+  
+  //     hook.afterEach(function(html, next) {
+  //       // 解析成 html 后调用。beforeEach 和 afterEach 支持处理异步逻辑
+  //       // ...
+  //       // 异步处理完成后调用 next(html) 返回结果
+  //       next(html)
+  //       console.debug("doneEach")
+  //     })
+  
+  //     hook.doneEach(function() {
+  //       // 每次路由切换时数据全部加载完成后调用，没有参数。
+  //       // ...
+  //       console.debug("doneEach")
+  //     })
+  
+  //     hook.ready(function() {
+  //       // 初始化并第一次加载完成数据后调用，没有参数。
+  //       console.debug("ready")
+  //     })
+  //   }
+  //  ]
 }
 
