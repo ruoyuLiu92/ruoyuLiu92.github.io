e45bde9a1120ea3f7962d4ad3eb3351c453d5e04
kirito <1026860069@qq.com>
2020-04-21 12:14:26 +0800

    test

--- js/gitalk-my.js
+++ js/gitalk-my.js
@@ -1,4 +1,4 @@
-const gitalk = new Gitalk({
+gitalk = new Gitalk({
     clientID: 'ced6988e6ccedf962b67',
     clientSecret: '73a32399b7eda1c3142aa163775550aa214b056a',
     repo: 'zshorz.github.io',
@@ -11,9 +11,9 @@ const gitalk = new Gitalk({
 function lablegen(hook) {
     var dom = Docsify.dom;
 
-    hook.doneEach(function (_) {
+    hook.doneEach(function () {
         gitalk.id = hex_md5(hex_md5(window.location.pathname + window.location.hash))
-        //console.log(gitalk.id)
+        console.log(gitalk.id)
     });
 }
 //
